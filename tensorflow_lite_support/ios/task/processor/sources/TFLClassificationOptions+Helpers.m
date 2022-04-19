b7b3edd3491ae4dc9b45034c5908e9edfaeb3c2e
Prianka Liz Kariat <prianka.kariat@ymedialabs.com>
2022-04-20 00:42:06 +0530

    Fixed allocation of displayNamesLocale

--- tensorflow_lite_support/ios/task/processor/sources/TFLClassificationOptions+Helpers.m
+++ tensorflow_lite_support/ios/task/processor/sources/TFLClassificationOptions+Helpers.m
@@ -75,13 +75,11 @@
     cClassificationOptions->label_allowlist.length = (int)self.labelAllowList.count;
   }
 
-  if (self.displayNamesLocale) {
     if (self.displayNamesLocale.UTF8String) {
-      cClassificationOptions->display_names_local = strdup(self.displayNamesLocale.UTF8String);
-      if (!cClassificationOptions->display_names_local) {
-        exit(-1);  // Memory Allocation Failed.
-      }
-    } else {
+      // strdup is not needed as C layer handles copying (C++ options are protobufs).
+      // Hence setting char* values in protobuf leads to copying.
+      cClassificationOptions->display_names_local = self.displayNamesLocale.UTF8String; 
+    }
       [TFLCommonUtils createCustomError:error
                                withCode:TFLSupportErrorCodeInvalidArgumentError
                             description:@"Could not convert (NSString *) to (char *)."];
